2bb2acecbe2097747c018353e6f8cf1b8a3e7bd0
Gift Goma <gomagiftk01@gmail.com>
2024-01-06 20:55:11 +0200

    init

---
+++ Chapter_1/Exercise_1.java
@@ -0,0 +1,17 @@
+
+ package Chapter_1;
+ 
+ /**
+* Exercise_1
+ * (Display three messages) Write a program that displays Welcome to Java, Learning Java Now, and Programming is fun.
+ */
+
+
+ public class Exercise_1 {
+   public static void main(String[] args) {
+    System.out.println("Welcome to Java");
+    System.err.println("Learning Java Now");
+    System.out.println("Programming is fun");
+   }
+    
+ }
